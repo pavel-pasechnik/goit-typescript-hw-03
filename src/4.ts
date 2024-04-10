0be5c6b47217187ba5bdefce6562560736992bc0
Pavel <Pavel.Pasechnik@gmail.com>
2024-04-10 14:01:59 +0300

    added homework

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,42 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  private tenants: Person[] = [];
+
+  constructor(protected key: Key) {
+    this.door = false;
+  }
+
+  comeIn(person: Person) {
+    this.door === true && this.tenants.push(person);
+  }
+
+  OpenDoor(key: Key): void {}
+}
+
+class MyHouse extends House {
+  openDoor(theKey: Key) {
+    theKey.getSignature() === this.key.getSignature() && (this.door = true);
+  }
+}
+
+//
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +45,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
